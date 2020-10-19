21fbbe074c17203564b11ac6a3389d46050ac65f
JeanneSic <siche.jeanne@gmail.com>
2020-10-19 22:07:24 +0200

    posting solution composer-2

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'c7f21d9e738c19403631b7bdbfc6fdaf48933f18',
+    'name' => 'jeannesiche/composer-quete',
+  ),
+  'versions' => 
+  array (
+    'ehime/hello-world' => 
+    array (
+      'pretty_version' => '1.0.5',
+      'version' => '1.0.5.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b1c8cdd2c11272d8c5deec7816e51fa5374217c1',
+    ),
+    'jeannesiche/composer-quete' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'c7f21d9e738c19403631b7bdbfc6fdaf48933f18',
+    ),
+  ),
+);
